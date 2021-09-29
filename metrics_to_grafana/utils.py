a2bd4d94ebcd05ed3a6e3c5f91037f0c0f7f01da
Ebad Kamil <ebad.kamil@ess.eu>
2021-09-29 13:06:18 +0200

    Add graylog handler to logger

--- metrics_to_grafana/utils.py
+++ metrics_to_grafana/utils.py
@@ -1,7 +1,9 @@
 import logging
 
+import graypy
 
-def get_logger(name, level: int = logging.DEBUG):
+
+def get_logger(name, graylog_logger_address, level: int = logging.DEBUG):
     logger = logging.getLogger(name)
     console_handler = logging.StreamHandler()
     formatter = logging.Formatter(
@@ -10,5 +12,10 @@ def get_logger(name, level: int = logging.DEBUG):
     console_handler.setFormatter(formatter)
 
     logger.addHandler(console_handler)
+
+    host, port = graylog_logger_address.split(":")
+    handler = graypy.GELFTCPHandler(host, int(port))
+    logger.addHandler(handler)
+
     logger.setLevel(level)
     return logger
